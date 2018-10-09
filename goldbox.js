a56c9317448d353c418342d7d9887ef834a95da5
Colby Fayock <fayock@gmail.com>
2018-10-08 20:59:20 -0400

    cleaning up the product creation piece

--- goldbox.js
+++ goldbox.js
@@ -1,10 +1,16 @@
 const request = require('request');
 const awssdk = require('aws-sdk');
+const axios = require('axios');
 const s3 = new awssdk.S3();
 
 const AmazonRSS = require('./lib/amazon-rss');
 const Util = require('./lib/util');
 const Kenny = require('./lib/kenny');
+
+Kenny.set({
+  service: process.env.service,
+  function: 'goldbox'
+});
 
 /**
  * goldbox
@@ -14,62 +20,55 @@ const Kenny = require('./lib/kenny');
 
 module.exports.goldbox = function(event, context) {
 
-    Kenny.set({
-        service: process.env.service,
-        function: 'goldbox'
-    });
+  Kenny.log(`RSS Get - ${process.env.rss_feed}`);
 
-    Kenny.log(`RSS Get - ${process.env.rss_feed}`);
+  axios.get(process.env.rss_feed)
+    .then(response => {
 
-    // Make a request to the RSS feed per the config
+      Kenny.log(`RSS Success - ${process.env.rss_feed}`);
 
-    request(process.env.rss_feed, {}, (error, response, body) => {
+      const widget_scripts = AmazonRSS.getScriptsFromHtml(response.data);
+      const deals = AmazonRSS.getDealsFeedFromScripts(widget_scripts, {
+          affiliate_id: process.env.affiliate_id
+      });
 
-        if ( error ) {
-            Util.respond({
-                callback: event,
-                status_code: 500,
-                message: Kenny.log(`RSS Error - ${JSON.stringify(error)}`)
-            });
-            return;
-        }
+      Kenny.log(`S3 Put - ${process.env.bucket}/${process.env.goldbox_path}`);
 
-        Kenny.log(`RSS Success - ${process.env.rss_feed}`);
+      // Once we have the desired feed, dump it into an S3 bucket
 
-        var widget_scripts = AmazonRSS.getScriptsFromHtml(body);
-        var deals = AmazonRSS.getDealsFeedFromScripts(widget_scripts, {
-            affiliate_id: process.env.affiliate_id
-        });
+      s3.putObject({
+          Bucket: process.env.bucket,
+          Key: process.env.goldbox_path,
+          Body: JSON.stringify(deals),
+      }, function(s3_error, s3_data) {
 
-        Kenny.log(`S3 Put - ${process.env.bucket}/${process.env.goldbox_path}`);
+          if ( s3_error ) {
+              Util.respond({
+                  callback: event,
+                  status_code: 500,
+                  message: Kenny.log(`S3 Error - ${JSON.stringify(s3_error)}`)
+              });
+              return;
+          }
 
-        // Once we have the desired feed, dump it into an S3 bucket
+          // Finally respond with a 200
 
-        s3.putObject({
-            Bucket: process.env.bucket,
-            Key: process.env.goldbox_path,
-            Body: JSON.stringify(deals),
-        }, function(s3_error, s3_data) {
+          Util.respond({
+              callback: event,
+              status_code: 200,
+              message: Kenny.log(`S3 Success - ${JSON.stringify(s3_data)}`)
+          });
 
-            if ( s3_error ) {
-                Util.respond({
-                    callback: event,
-                    status_code: 500,
-                    message: Kenny.log(`S3 Error - ${JSON.stringify(s3_error)}`)
-                });
-                return;
-            }
+      });
 
-            // Finally respond with a 200
+    })
+    .catch(error => {
 
-            Util.respond({
-                callback: event,
-                status_code: 200,
-                message: Kenny.log(`S3 Success - ${JSON.stringify(s3_data)}`)
-            });
-
-        });
+      Util.respond({
+        callback: event,
+        status_code: 500,
+        message: Kenny.log(`RSS Error - ${error}`)
+      });
 
     });
-
 }
